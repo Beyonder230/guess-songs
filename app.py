f0a3b74949cd6393bb3e55af302152ea74dd5e9d
Bruno Vilas Boas Fernandes <brunovbf2006@gmail.com>
2025-05-19 22:26:19 -0300

    Added deezer API compatibility, added a function to get possible answers for the game, style singleplayer html page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 import requests
 from flask import Flask, render_template, session, request, redirect
-from helpers import get_access, get_random_track, get_id_from_url, test_access
+from helpers import get_access, get_random_track, get_options
 
 app = Flask(__name__)
 app.secret_key = ***REMOVED***
@@ -24,21 +24,29 @@ def singleplayer():
     if "singleplayer_score" not in session:
         session.permanent = True
         session["singleplayer_score"] = 0
+        
     token = get_access()
     if token == None:
         return "auth error"
     
-    playlist_url = "https://open.spotify.com/playlist/09tJur2DIOM35bqoVXDjgw?si=df9b3362229f47f1"
-    playlist_id = get_id_from_url(playlist_url, "playlist")
-    if playlist_id == None:
-        return "wrong playlist"
+    playlist_url = "https://www.deezer.com/br/playlist/4461060364"
     
     # test_access(token)
-    track = get_random_track(playlist_id, token)
+    track = get_random_track(playlist_url, token)
     print(f"return of get_random_track(): {track}")
+    if track == "error":
+        return redirect("/error")
     
-    return render_template("singleplayer.html", bigger_score = session["singleplayer_score"])
+    options = get_options(playlist_url, track)
+    print(f"return of get_options(): {options}")
+    if options == "error":
+        return redirect("/error")
+    
+    return render_template("singleplayer.html", bigger_score = session["singleplayer_score"], song = track["preview"], options = options)
 
+@app.route("/error")
+def error():
+    return render_template("error.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
