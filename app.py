e9fc57394f4fd3cada86b86632e5edf9b73a56e9
Bruno Vilas Boas Fernandes <brunovbf2006@gmail.com>
2025-05-31 13:38:07 -0300

    Added custom gamemode and improved frontend design for gamemodes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from datetime import timedelta
-import requests
+import urllib.parse
 import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, session, request, redirect, url_for
-from helpers import get_access, get_random_track, get_options
+from helpers import get_access, get_deezer_options, get_spotify_options, get_random_deezer_track, get_random_spotify_track
 
 load_dotenv()
 
@@ -15,57 +15,169 @@ app.permanent_session_lifetime = timedelta(days=99)
 def index():
     return render_template("index.html")
 
+
+
+
 @app.route('/howthegameworks')
 def howthegameworks():
     return render_template("rules.html")
+
+
+
 
 @app.route('/gamemodes')
 def play():
     return render_template("gamemodes.html")
 
+
+
+
 @app.route('/singleplayer', methods=["GET", "POST"])
 def singleplayer():
-    if request.method == "POST":
-        data = request.get_json()
-        title = data.get("current_title")
-        new_score = data.get("current_score")
-        
-    if "singleplayer_score" not in session:
+    if "best_singleplayer_score" not in session:
         session.permanent = True
-        session["singleplayer_score"] = 0
-        
-    token = get_access()
-    if token == None:
-        return "auth error"
-    
+        session["best_singleplayer_score"] = 0
+    else:
+        if session["singleplayer_score"] > session["best_singleplayer_score"]:
+            session["best_singleplayer_score"] = session["singleplayer_score"]
+     
     # default playlist for this gamemode
     playlist_url = "https://www.deezer.com/br/playlist/4461060364"
-    
-    # test_access(token)
-    track = get_random_track(playlist_url, token)
+    track = get_random_deezer_track(playlist_url)
         
-    print(f"return of get_random_track(): {track}")
+    # print(f"return of get_random_track(): {track}")
     if track == "error":
         return redirect("/error")
     
     # get info about the answer options
-    options = get_options(playlist_url, track)
-    print(f"return of get_options(): {options}")
+    options = get_deezer_options(playlist_url, track)
+    # print(f"return of get_options(): {options}")
     if options == "error":
         return redirect("/error")
-    
-    if request.method == "POST":
-        return redirect(url_for("singleplayer", score = new_score))
-    else:
-        return render_template("game.html", bigger_score = session["singleplayer_score"], song = track, options = options, score = 0)
+
+    return render_template("game.html", bigger_score = session["best_singleplayer_score"], song = track, options = options, score = session["singleplayer_score"], gamemode = "singleplayer")
+
+
+
 
 @app.route("/error")
 def error():
     return render_template("error.html")
 
+
+
+
 @app.route("/attributions")
 def attributions():
     return render_template("attributions.html")
+
+
+
+
+@app.route("/score_update/custom/<path:url>")
+def score_custom(url):
+    mode_score = "custom_score"
+    
+    if mode_score not in session:
+        session[mode_score] = 0
+    
+    session[mode_score] += 1
+    
+
+    url = urllib.parse.quote_plus(url, safe='')
+        
+    return redirect(url_for("custom", url=url))
+    
+    
+    
+    
+@app.route("/score_update/<string:gamemode>")
+def score_update(gamemode):
+    mode_score = f"{gamemode}_score"
+    
+    if mode_score not in session:
+        session[mode_score] = 0
+    
+    session[mode_score] += 1
+    
+    if gamemode == "custom":
+        url = urllib.parse.quote_plus(url, safe='')
+            
+        return redirect(url_for(gamemode, url=url))
+    else:
+        return redirect(url_for(gamemode))
+
+
+
+
+@app.route("/score_reset/<string:gamemode>", methods=["GET", "POST"])
+def score_reset(gamemode):
+    mode_score = f"{gamemode}_score"
+    
+    session[mode_score] = 0
+    
+    if mode_score not in session:
+        session[mode_score] = 0
+        
+    if request.method == "GET":
+        return redirect("/gamemodes")
+    elif request.method == "POST":
+        if gamemode == "custom":
+            url = request.form.get("url")
+            url = urllib.parse.quote_plus(url, safe='')
+            
+            print(f"url: {url}")
+            return redirect(url_for(gamemode, url=url))
+        else:
+            return redirect(url_for(gamemode))
+        
+        
+        
+        
+@app.route("/custom/<string:url>", methods=["GET", "POST"])
+def custom(url): 
+    url = urllib.parse.unquote_plus(url)
+    
+    if "best_custom_score" not in session:
+        session.permanent = True
+        session["best_custom_score"] = 0
+    if "custom_score" not in session:
+        session["custom_score"] = 0
+
+    if session["custom_score"] > session["best_custom_score"]:
+        session["best_custom_score"] = session["custom_score"]
+     
+    if "spotify" in url:
+        token = get_access()
+        if token == None:
+            return "auth error"
+    
+    if "spotify" in url:
+        track = get_random_spotify_track(url, token)
+    elif "deezer" in url:
+        track = get_random_deezer_track(url)
+    else:
+        print(f"url: {url}")
+        return redirect("/error") # temporary!!!!!!
+        
+    # print(f"return of get_random_track(): {track}")
+    if track == "error":
+        return redirect("/error")
+    
+    # get info about the answer options
+    if "spotify" in url:
+        options = get_spotify_options(url, track)
+    elif "deezer" in url:
+        options = get_deezer_options(url, track)
+    else:
+        print(f"url: {url}")
+        return redirect("/error") # temporary!!!!!!
+
+    # print(f"return of get_options(): {options}")
+    if options == "error":
+        return redirect("/error")
+
+    return render_template("game.html", bigger_score = session["best_custom_score"], song = track, options = options, score = session["custom_score"], gamemode = "custom", url=url)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -9,3 +9,15 @@ function openConfig() {
 function closeConfig() {
     document.getElementById("settings").style.display = "none";
 }
+
+function openCustom() {
+    if (document.getElementById("custom").style.display != "block") {
+        document.getElementById("custom").style.display = "block";
+    } else {
+        document.getElementById("custom").style.display = "none";
+    }
+}
+
+function closeCustom() {
+    document.getElementById("custom").style.display = "none";
+}
